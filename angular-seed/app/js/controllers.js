7d4445113e2de7fc49ebd1ea25a338a98f7cc161
asood <atinsood@gmail.com>
2013-05-16 22:17:16 -0400

    end to end working with several bugs

--- angular-seed/app/js/controllers.js
+++ angular-seed/app/js/controllers.js
@@ -73,26 +73,36 @@ function MyCtrl1($scope, $http) {
 
 function MyCtrl2($scope, $http) {
 
-    $scope.rows = []
-    $scope.schemas = []
-
     $http.get('http://localhost:8888/dynamicUI').success(function (data) {
-            console.log("Got schemas");
-            console.log(data);
 
+            //add an index to each element so that its easy to look it up back in array
+            data.forEach(function (element, index, array) {
+                element['index'] = index
+            })
             $scope.schemas = data
-            //currently hardcoding to schema 1 only
-            var schema1 = data[0]
-            schema1['fields'].forEach(generateUI)
 
-            function generateUI(element, index, array) {
-                $scope.rows.push(new Object({'name': element['name'], 'value': ""}));
-
-            }
 
         }
-    )
-    ;
+    );
+
+
+    $scope.go = function () {
+        $http.get('http://localhost:8888/dynamicUI').success(function (data) {
+
+                var schema = data[$scope.schemaName['index']]
+
+                //clean up the table showing the form and then repopulate it
+                $scope.rows = []
+                schema['fields'].forEach(generateUI)
+                function generateUI(element, index, array) {
+                    $scope.rows.push(new Object({'name': element['name'], 'value': ""}));
+
+                }
+
+            }
+        );
+    }
+
 
     $scope.submit = function () {
     }
